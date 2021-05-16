7515b37c8c1128baf39071bc77ade8c7dd2385c0
LuxuryFi <anhit.luxuryfi@gmail.com>
2021-05-16 20:44:54 +0700

    First push:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const categoryRouter = require('./category')
 const tagRouter = require('./tag')
 const userRouter = require('./user')
+const postRouter = require('./post')
 
 function route(app) {
     
     app.use('/category',categoryRouter);
     app.use('/tag',tagRouter);
     app.use('/user',userRouter);
+    app.use('/post',postRouter);
 
     app.get('/', (req,res,next) => {
         res.render('home')
